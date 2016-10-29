d87af5952c6d088ec175f12207a7bd2b3b19e849
hklucher <hklucher@gmail.com>
2016-10-29 14:41:02 -0500

    ruidmentary index page for quizzes

--- web/controllers/quiz_controller.ex
+++ web/controllers/quiz_controller.ex
@@ -1,7 +1,14 @@
 defmodule MusicQuiz.QuizController do
   use MusicQuiz.Web, :controller
+  alias MusicQuiz.Repo
+  alias MusicQuiz.Genre
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  def index(conn, %{"genre_id" => genre_id}) do
+    # TODO: It makes sense to display quizzes on a genre show page. Move this?
+    genre = Repo.get(Genre, genre_id) |> Repo.preload(:quizzes)
+    conn
+    |> assign(:genre, genre)
+    |> assign(:quizzes, genre.quizzes)
+    |> render "index.html"
   end
 end
