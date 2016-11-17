7cb320fba0160eb6205c28b4e0a212ff1039bd6f
Harrison Klucher <hklucher@gmail.com>
2016-11-17 11:33:23 -0600

    seed setup

--- priv/repo/seeds/albums.exs
+++ priv/repo/seeds/albums.exs
@@ -1,10 +1,36 @@
+require IEx;
 defmodule MusicQuiz.Seeds.Tracks do
+  alias MusicQuiz.Spotify
+  alias MusicQuiz.Track
+  alias MusicQuiz.Repo
+  @moduledoc """
+  Module to seed track data from Spotify into DB
+  """
+
   def seed(%{"album_range" => albums}) do
+    Enum.each(albums, fn(album) ->
+      {:ok, %{"href" => _, "items" => tracks}} = Spotify.album_tracks(album.spotify_id)
+      Enum.each(tracks, fn(track) ->
+        changeset = build_changeset_from_track(track)
+        IEx.pry
+      end)
+    end)
     # FOR EACH album IN albums
       # GET list of tracks from Spotify
-      # FOR EACH track IN tracks
-        # INSERT track, with association to album
-      # END FOR
+      # IF response ~ 200
+        # FOR EACH track IN tracks
+          # INSERT track, with association to album
+          # Slumber!
+        # END FOR
+      # ELSE
+        # Terminate program
     # END FOR
+  end
+
+  defp build_changeset_from_track(track) do
+    data = track
+           |> Map.take(["name", "preview_url", "duration_ms", "track_number"])
+           |> Map.put("spotify_id", track["id"])
+    Track.changeset(%Track{}, data)
   end
 end
