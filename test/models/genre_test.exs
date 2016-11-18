53b31da6c864cd99829e913facea86f2a4ff5990
Harrison Klucher <hklucher@gmail.com>
2016-11-18 15:30:54 -0600

    set up ex_machina, more tests for genre

--- test/models/genre_test.exs
+++ test/models/genre_test.exs
@@ -2,7 +2,23 @@ defmodule MusicQuiz.GenreTest do
   use MusicQuiz.ModelCase
   alias MusicQuiz.Genre
 
-  test "it runs" do
-    assert true
+  import MusicQuiz.Factory
+
+  test "with valid attributes" do
+    changeset = Genre.changeset(%Genre{name: "ska"})
+    assert changeset.valid?
+  end
+
+  test "with invalid attributes" do
+    changeset = Genre.changeset(%Genre{name: ""})
+    refute changeset.valid?
+  end
+
+  test ".albums returns all albums written by artists of that genre" do
+    genre = insert(:genre)
+    artist_for_genre = insert(:artist, genres: [genre])
+    album_for_genre = insert(:album, artist: artist_for_genre)
+    album_titles_by_genre = Genre.albums(genre) |> Enum.map(fn(x) -> x.name end)
+    assert Enum.member?(album_titles_by_genre, album_for_genre.name)
   end
 end
---
+++ test/support/factory.ex
@@ -0,0 +1,26 @@
+defmodule MusicQuiz.Factory do
+  use ExMachina.Ecto, repo: MusicQuiz.Repo
+
+  def genre_factory do
+    %MusicQuiz.Genre{
+      name: "ska"
+    }
+  end
+
+  def album_factory do
+    %MusicQuiz.Album{
+      name: "Why do They Rock so Hard?",
+      image_url: "www.example.com",
+      spotify_id: "123abc"
+    }
+  end
+
+  def artist_factory do
+    %MusicQuiz.Artist{
+      name: "Reel Big Fish",
+      popularity: 99,
+      image_url: "www.example.com",
+      spotify_id: "abc123"
+    }
+  end
+end
