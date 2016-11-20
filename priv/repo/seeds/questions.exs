5bbf9af19bfcb6fa1063f4d659dd35e210d7cbda
Harrison Klucher <hklucher@gmail.com>
2016-11-20 13:23:23 -0600

    move artists seeds to separate module

--- priv/repo/seeds/questions.exs
+++ priv/repo/seeds/questions.exs
@@ -1,23 +1,33 @@
 defmodule MusicQuiz.Seeds.Questions do
-  alias MusicQuiz.{Repo, Genre, Quiz}
+  alias MusicQuiz.{Repo, Genre, Quiz, Album, Question}
 
   def seed("album_authors") do
     Enum.each(Repo.all(Quiz), fn(quiz) ->
-      genre = quiz_genre(quiz) |> Repo.preload(:artist)
+      genre = quiz_genre(quiz) |> Repo.preload(:artists)
       albums = Genre.albums(genre)
       Enum.each(albums, fn(album) -> insert_match_song_to_album(album) end)
     end)
   end
 
   defp insert_match_song_to_album(album) do
-    # Get list of songs belonging to album
-      # Select ONE at random
-    # Get list of songs NOT belonging to album
-      # Select THREE at random
-    # Insert question "What album includes the song x?"
-    # Insert random song from album as answer to question
-    # Insert random songs not from album as responses to question
+    correct_answer = Album.tracks(album) |> Enum.random
+    distractors = Album.not_owned_tracks(album.id) |> Enum.take_random(3)
+    changeset = Question.changeset(%Question{content: "What album includes the song #{correct_answer.name}"})
+    case Repo.insert(changeset) do
+      {:ok, changeset} ->
+        changeset = changeset |> Repo.preload([:answer, :responses])
+        changeset
+        |> Ecto.Changeset.change
+        |> Ecto.Changeset.put_assoc(:answer, correct_answer)
+        |> Ecto.Changeset.put_assoc(:responses, changeset.responses ++ distractors)
+        |> Repo.update!
+      {:error, changeset} ->
+        IO.inspect changeset.errors
+        System.halt(0)
+    end
   end
 
   defp quiz_genre(quiz), do: (quiz |> Repo.preload(:genre)).genre
 end
+
+MusicQuiz.Seeds.Questions.seed("album_authors")
