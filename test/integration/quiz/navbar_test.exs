b55dd3b391beb1daf968c66882e273a23785ace0
Harrison Klucher <hklucher@gmail.com>
2016-11-29 15:46:45 -0600

    link to log in

--- test/integration/quiz/navbar_test.exs
+++ test/integration/quiz/navbar_test.exs
@@ -1,6 +1,6 @@
 defmodule MusicQuiz.NavbarTest do
-  use Hound.Helpers
   use MusicQuiz.ConnCase
+  use Hound.Helpers
 
   import MusicQuiz.Factory
   import MusicQuiz.TestHelpers
@@ -9,12 +9,23 @@ defmodule MusicQuiz.NavbarTest do
 
   test "has a link to sign up when not logged in" do
     navigate_to("/")
-    refute find_element(:link_text, "Sign Up") == nil
+    assert find_element(:link_text, "Sign Up")
   end
 
   test "does not link to sign up when logged in" do
     user = insert(:user)
     login(user)
-    assert {:error, _} = search_element(:link_text, "Sign Up", 1)
+    assert {:error, _} = search_element(:link_text, "Sign Up", 0)
+  end
+
+  test "does not have a link to log out when logged in" do
+    user = insert(:user)
+    login(user)
+    assert {:error, _} = search_element(:link_text, "Sign Out", 0)
+  end
+
+  test "has a link to log in when not logged in" do
+    navigate_to("/")
+    assert {:ok, _} = search_element(:link_text, "Log In", 0)
   end
 end
