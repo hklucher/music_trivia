46cf3bcf7de2344beb061bc361b77185a1d649be
Harrison Klucher <hklucher@gmail.com>
2016-12-22 09:27:21 -0600

    update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -10,7 +10,6 @@ use Mix.Config
 # or recreate for your teammates (or you later on).
 config :music_quiz, MusicQuiz.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
-  # secret_key_base: "C/qfYtpIaFwsyXxfxal1n1w7kyoGNHoBbwBchkhfykIGSIeGL/h0qj+jaNO2yEyf"
 
 # Configure your database
 config :music_quiz, MusicQuiz.Repo,
@@ -21,7 +20,3 @@ config :music_quiz, MusicQuiz.Repo,
   # password: "postgres",
   database: "music_quiz_prod",
   pool_size: 20
-
-config :music_quiz,
-  spotify_client_id: "ccbf2cf9496844b48eaa89100cb97968",
-  spotify_client_secret: "cda951c9804b4a27bdf9ce4509a48268"
