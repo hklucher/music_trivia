de4d4dd0d7cabcb4177a321be90b4482df2401cf
Harrison Klucher <hklucher@gmail.com>
2017-01-05 16:00:09 -0600

    display full name when user has a first and last name

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,4 +1,3 @@
-require IEx
 defmodule MusicQuiz.UserController do
   use MusicQuiz.Web, :controller
   use Guardian.Phoenix.Controller
--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,7 +1,16 @@
 defmodule MusicQuiz.UserView do
   use MusicQuiz.Web, :view
+  
+  alias MusicQuiz.Repo
+  alias MusicQuiz.User
 
   def render("show.json", %{user: user}) do
-    Map.take(user, [:id, :email])   
+    Map.take(user, [:id, :email])
   end
+
+  def display_name(%User{first_name:  first_name, last_name: last_name}) do
+    "#{first_name} #{last_name}"
+  end
+
+  def display_name(%User{email: email}), do: email
 end
